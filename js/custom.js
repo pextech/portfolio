1a165a9933b143c94d2cc3114f3ac5058286a4e8
Mupenzi <mcstain1639@gmail.com>
2020-11-09 21:48:56 +0200

    ft-main everything on update

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+   
+    $("a.mobile").click(function(){
+        $(".sidebar").slideToggle('slow');
+    });
+
+    window.onresize = function(event){
+        if ($(window).width() > 857 ){
+            $(".sidebar").show();
+        }
+    }
+    
+});
+$(document).ready(function(){
+
+    $('#createPost').click(function(){
+        $("#addPage").slideToggle("slow");
+    });
+    $("#close").click(function(){
+        $("#addPage").slideToggle("slow");
+    });
+    $("#closeModal").click(function(){
+        $("#addPage").slideToggle("slow");
+    });
+
+    
+});
+
+$(document).ready(function(){
+
+    $('#createUser').click(function(){
+        $("#addUser").slideToggle("slow");
+    });
+    $("#close").click(function(){
+        $("#addUser").slideToggle("slow");
+    });
+    $("#closeModal").click(function(){
+        $("#addUser").slideToggle("slow");
+    });
+
+    
+});
